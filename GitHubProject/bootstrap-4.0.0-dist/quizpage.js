58347233f7977f80aa1dd626a7ae91779b225b73
StarProcyon <szabo.dominik@students.jedlik.eu>
2024-05-15 14:16:34 +0200

    second quiz update

--- GitHubProject/bootstrap-4.0.0-dist/quizpage.js
+++ GitHubProject/bootstrap-4.0.0-dist/quizpage.js
@@ -9,11 +9,11 @@ function checkAnswers() {
     } else {
         resultsHtml += '<p class="incorrect">A válaszod helytelen az első kérdésre. Gergő beceneve Jamba!</p>';
     }
-    if (answer2 === '9') {
+    if (answer2 === '92008.09.09.') {
         resultsHtml += '<p class="correct">Válaszod a második kérdésre helyes!</p>';
         score++;
     } else {
-        resultsHtml += '<p class="incorrect">Question 2 is incorrect. The correct answer is 9.</p>';
+        resultsHtml += '<p class="incorrect">A válaszod a második kérdésre helytelen. A helyes válasz 2008.09.09.</p>';
     }
     resultsHtml += '<p>Your score is ' + score + ' out of 2.</p>';
     document.getElementById('results').innerHTML = resultsHtml;
